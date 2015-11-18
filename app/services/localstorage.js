1f42e1e35d2c719c7f7dcde88270cf211abe963d
Pompeu <itacir@hotmail.com>
2015-11-18 17:52:01 -0200

    initial commit

---
+++ app/services/localstorage.js
@@ -0,0 +1,39 @@
+(function() {
+  angular.module('localstorage',[])
+    .factory('localstorage', localstorage);
+
+    localstorage.$intjec = ['$window'];
+
+    function localstorage ($window) {
+      var service = {
+        set : set,
+        get : get,
+        put : put,
+        del : del
+      };
+
+      return service;
+
+      function set (key, value) {
+        $window.localStorage.setItem(key, JSON.stringify(value));
+        var result = get(key);
+        return result;
+      }
+
+      function get (key) {
+        var result = $window.localStorage.getItem(key);
+        return JSON.parse(result);
+      }
+
+      function put (key, value) {
+        del(key);
+        return set(key, value);
+      }
+
+      function del (key) {
+        $window.localStorage.removeItem(key);
+        return get(key)? false : true;
+      }
+    }
+
+}());
---
+++ testes/storage.Spec.js
@@ -0,0 +1,36 @@
+'use strict';
+
+describe('testes of localstorage module', function() {
+
+  var service = null;
+  beforeEach(module('localstorage'));
+
+  beforeEach(inject(function(_localstorage_){
+    service = _localstorage_;
+  }));
+
+  describe('should be have function set', function () {
+    it('should be set function return a object', function () {
+      var beer = {name : "Skol" };
+      var beerSaved = service.set('beers',beer);
+      expect(beer).toEqual(beerSaved);
+    });
+
+    it('should be get function return a object', function () {
+      var beer = {name : "Skol" };
+      var beerSaved = service.get('beers');
+      expect(beer).toEqual(beerSaved);
+    });
+
+    it('should be put function return a updated object', function () {
+      var newBeer = {name : "Skol Beats" };
+      var beerSaved = service.put('beers', newBeer);
+      expect(newBeer).toEqual(beerSaved);
+    });
+
+    it('should be del function remove a object', function () {
+      var isRemoved = service.del('beers');
+      expect(true).toEqual(isRemoved);
+    });
+  });
+});
